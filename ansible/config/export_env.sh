c2f5af674ec061c4bbaf73bae531b5a09b734bc1
jostaub <67969701+jostaub@users.noreply.github.com>
2020-10-05 15:10:32 +0200

    moved to setup folder

--- ansible/config/export_env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Export ANSIBLE_CONFIG for Project"
-export ANSIBLE_CONFIG="$(pwd)/ansible.cfg"
---
+++ setup/export_env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Export ANSIBLE_CONFIG for Project"
+export ANSIBLE_CONFIG="$(pwd)/../ansible/config/ansible.cfg"
