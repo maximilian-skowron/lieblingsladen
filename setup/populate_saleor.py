9b19f69f0add571b82a8303a5d602b269f28c235
jostaub <67969701+jostaub@users.noreply.github.com>
2020-10-06 18:16:53 +0200

    added products to setup

--- setup/populate_saleor.py
+++ setup/populate_saleor.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import random
 
 GRAPHQL_URL = 'http://localhost:8000/graphql/'
 GRAPHQL_BEAR_TOKEN = os.environ.get('GRAPHQL_BEAR_TOKEN')
@@ -25,6 +26,20 @@ def get_query_create_category(name: str, parrent_id: str = None):
                 }
             }
         }""" % (name, '' if parrent_id is None else parrent_part)
+
+    return query
+
+
+def get_querry_create_example_product(name: str, product_type_id: str, category_id: str):
+    lorem_ipsum = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. "
+
+    query = """mutation exampleProduct {
+            productCreate (input: {name:"%s", productType:"%s", category:"%s", basePrice:"%s", description:"%s", isPublished:true}){
+                product {
+                name
+                }
+            }
+        }""" % (name, product_type_id, category_id, random.uniform(0.5, 9.9), lorem_ipsum)
 
     return query
 
@@ -60,7 +75,35 @@ def create_categories():
     return category_id_dict
 
 
+def create_products(category_dict):
+    products = [
+        ('Orangensaft', '', category_dict['Säfte']),
+        ('Apfelsaft', '', category_dict['Säfte']),
+        ('Wein', '', category_dict['Alkoholische Getränke']),
+        ('Bier', '', category_dict['Alkoholische Getränke']),
+        ('Banane', '', category_dict['Obst']),
+        ('Birne', '', category_dict['Obst']),
+        ('Gurke', '', category_dict['Gemüse']),
+        ('Tomate', '', category_dict['Gemüse']),
+        ('Blitzbringer', '', category_dict['Lieferservices']),
+        ('Windgeschwind', '', category_dict['Lieferservices']),
+        ('Super Lappen', '', category_dict['Reinigungsutensilien']),
+        ('Spüli', '', category_dict['Reinigungsutensilien']),
+        ('Klopapier', '', category_dict['Verbrauchsgüter']),
+        ('Zahnpasta', '', category_dict['Verbrauchsgüter']),
+        ('Trash Gera', '', category_dict['Veranstaltungen']),
+        ('Seven Club', '', category_dict['Veranstaltungen']),
+        ('Tierpark Gera', '', category_dict['Aktivitäten']),
+        ('Sky Motion Team', '', category_dict['Aktivitäten']),
+    ]
+
+    for product in products:
+        name, product_type_id, category_id = product
+        execute_querry(get_querry_create_example_product(
+            name, product_type_id, category_id))
+
+
 if __name__ == '__main__':
     category_dict = create_categories()
-
+    create_products(category_dict)
     pass
