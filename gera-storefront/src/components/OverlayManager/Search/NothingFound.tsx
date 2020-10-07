c841789e4f12d8d6b03c4d4a65e57f66dacafa21
jostaub <67969701+jostaub@users.noreply.github.com>
2020-10-07 13:37:13 +0200

    added translation + removed text

--- gera-storefront/src/components/OverlayManager/Search/NothingFound.tsx
+++ gera-storefront/src/components/OverlayManager/Search/NothingFound.tsx
@@ -3,11 +3,7 @@ import * as React from "react";
 export const NothingFound: React.FC<{ search: string }> = ({ search }) => (
   <div className="search__products--not-found">
     <p className="u-lead u-lead--bold u-uppercase">
-      Sorry, but we couldn’t match any search results for: {search}
-    </p>
-    <p>
-      Don’t give up - check the spelling, think of something less specific and
-      then use the search bar above.
+      Kein Produkt vorhanden für: {search}
     </p>
   </div>
 );
