8bf441cb3ce890da23bc00270e2b68f6fedf1d9a
jostaub <67969701+jostaub@users.noreply.github.com>
2020-10-07 13:12:26 +0200

    changed discount text

--- gera-storefront/src/views/Home/Page.tsx
+++ gera-storefront/src/views/Home/Page.tsx
@@ -43,12 +43,12 @@ const Page: React.FC<{
         <div className="home-page__hero-text">
           <div>
             <span className="home-page__hero__title">
-              <h1>Final reduction</h1>
+              <h1>Eröffnungsrabatt</h1>
             </span>
           </div>
           <div>
             <span className="home-page__hero__title">
-              <h1>Up to 70% off sale</h1>
+              <h1>bis 10% auf jedes Produkt</h1>
             </span>
           </div>
         </div>
